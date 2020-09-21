77c127ed0afefcc2df4a5668674c41972d54f897
Shane McDonough <shanemcdman@gmail.com>
2020-09-20 22:18:11 -0700

    Comment Node.cpp and .h

--- include/Node.h
+++ include/Node.h
@@ -3,21 +3,21 @@
 
 class Node {
 private:
-    char val;
-    Node* next;
-    Node* prev;
+    char val; // value stored in node
+    Node* next; // pointer to next node
+    Node* prev; // pointer to previous node
 public:
-    Node();
-    Node(char v);
-    void set_val(char v);
-    void set_next(Node* n);
-    void set_prev(Node* n);
-    char get_val();
-    Node* get_next();
-    Node* get_prev();
-    void insert(Node* new_node);
-    void insert_before(Node* new_node);
-    void remove();
+    Node(); // constructor
+    Node(char v); // constructor that intializes val
+    void set_val(char v); // sets val
+    void set_next(Node* n); // sets next
+    void set_prev(Node* n); // sets prev
+    char get_val(); // returns val
+    Node* get_next(); // returns next
+    Node* get_prev(); // returns prev
+    void insert(Node* new_node); // insert node after
+    void insert_before(Node* new_node); // insert node before
+    void remove(); // delete node
 };
 
 #endif /* NODE_H */
--- src/Node.cpp
+++ src/Node.cpp
@@ -1,65 +1,58 @@
 #include<Node.h>
 
-// private
+Node::Node():Node('A'){} // call constructor with val defaulting to 'A'
 
-
-
-// Public
-
-Node::Node():Node('A'){
+Node::Node(char v){ // 
+    val = v; // set val equal to v
+    next = prev = nullptr; // set next and prev equal to nullptr
 }
 
-Node::Node(char v){
-    val = v;
-    next = prev = nullptr;
-}
-
-void Node::set_val(char v){
+void Node::set_val(char v){ // set val
     val = v;
 }
 
-void Node::set_next(Node* n){
+void Node::set_next(Node* n){ // set next
     next = n;
 }
 
-void Node::set_prev(Node* n){
+void Node::set_prev(Node* n){ // set previous
     prev = n;
 }
 
-char Node::get_val(){
+char Node::get_val(){ // gets the value
     return val;
 }
 
-Node* Node::get_next(){
+Node* Node::get_next(){ // gets pointer to next
     return next;
 }
 
-Node* Node::get_prev(){
+Node* Node::get_prev(){ // gets pointer to previous
     return prev;
 }
 
-void Node::insert(Node* new_node){
-    if(next != nullptr){
-        new_node->set_next(next);
-        next->set_prev(new_node);
+void Node::insert(Node* new_node){ // insert a new node after
+    if(next != nullptr){ // if next exists
+        new_node->set_next(next); // set new_node's next to next
+        next->set_prev(new_node); // set next's previous to new_node
     }
-    next = new_node;
-    new_node->set_prev(this);
+    next = new_node; // set next to new_node
+    new_node->set_prev(this); // set new_node's previous to current node
 }
 
-void Node::insert_before(Node* new_node){
-    if(prev != nullptr){
-        new_node->set_prev(prev);
-        prev->set_next(new_node);
+void Node::insert_before(Node* new_node){ // insert new node before
+    if(prev != nullptr){ // if prev exists
+        new_node->set_prev(prev); // set new_node's prev to prev
+        prev->set_next(new_node); // set prev's next to new_node
     }
-    prev = new_node;
-    new_node->set_next(this);
+    prev = new_node; // set prev to new_node
+    new_node->set_next(this); // set new_node's next to current node
 }
 
-void Node::remove(){
-    if(prev != nullptr)
-        prev->set_next(next);
+void Node::remove(){ // delete current node
+    if(prev != nullptr) // if previous exists
+        prev->set_next(next); // set previous's next to next
     if(next != nullptr)
-        next->set_prev(prev);
-    delete this;
+        next->set_prev(prev); // set previous's prev to prev
+    delete this; // delete node
 }
