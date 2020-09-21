436f4eae325fd3cc918cbf755ad0a875eeca2f46
Shane McDonough <shanemcdman@gmail.com>
2020-09-20 22:08:05 -0700

    Comment class declaration

--- include/TextEditor.h
+++ include/TextEditor.h
@@ -7,27 +7,27 @@
 
 class TextEditor{
 private:
-    Node* head;
-    Node* curr;
-    bool running;
-    bool insert_at_begining;
-    void gotoxy(COORD coord);
-    COORD get_xy();
-    void keyboard_input();
-    void save_file(std::string file_name);
-    void load_file(std::string file_name);
-    std::string get_file_name();
-    void show_help();
-    void move_up();
-    void move_down();
-    void print_text();
-    void delete_list();
-    void insert_character(char key);
-    void delete_character();
+    Node* head; // pointing at first character in the document
+    Node* curr; // pointing at where the cursor is
+    bool running; // if the main loop is running
+    bool insert_at_begining; // if inserting before first character
+    void gotoxy(COORD coord); // move cursor to (x, y) coord on screen
+    COORD get_xy(); // get coord of cursor based on curr
+    void keyboard_input(); // take input from user
+    void save_file(std::string file_name); // save a current document to a file
+    void load_file(std::string file_name); // load document from a file
+    std::string get_file_name(); // get user input for file name
+    void show_help(); // print help screen
+    void move_up(); // move up one line maintaining x
+    void move_down(); // move up one line maintaining x
+    void print_text(); // print contents of document
+    void delete_list(); // delete the linked list containing document
+    void insert_character(char key); // insert character at curr
+    void delete_character(); // delete curr
 public:
-    TextEditor();
-    ~TextEditor();
-    void run();
+    TextEditor(); // constructor
+    ~TextEditor(); // deconstructor
+    void run(); // start running editor
 };
 
 #endif //TEXT_EDITOR
