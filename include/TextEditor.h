4ae146556ed2492d46f02fea00606c22831f7877
Shane McDonough <shanemcdman@gmail.com>
2020-09-22 10:24:25 -0700

    Print screen formatted but still fast

--- include/TextEditor.h
+++ include/TextEditor.h
@@ -32,7 +32,6 @@ private:
     void delete_clipboard();
     void copy_selection();
     void print_overlay();
-    void clear_non_overlay();
     std::pair<Node*, Node*> get_selection_start_end();
 public:
     TextEditor(); // constructor
